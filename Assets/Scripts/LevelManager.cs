bc376fe2f92af2d6e5cf52afa453eaf3a6c5b350
Quadrales <twhinchliffe1@gmail.com>
2025-07-29 01:39:39 +0100

    Updated LevelManager and level selection buttons to correctly load levels with ShapeManager on click. Functionality is not complete and contains bugs.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,23 +5,16 @@ public class LevelManager : MonoBehaviour
 {
     [SerializeField] private PuzzleData[] _levels;
     [SerializeField] private ShapeManager _shapeManager;
-    private int _currentIndex = 0;
 
-    void Start()
+    public void LoadLevel(int levelIndex)
     {
-        _shapeManager.LoadCurrentPuzzle(_levels[_currentIndex]);
+        _shapeManager.gameObject.SetActive(true);
+
+        _shapeManager.LoadCurrentPuzzle(_levels[levelIndex - 1]);
     }
 
     void Update()
     {
-        if (!_shapeManager.PuzzleComplete)
-        {
-            _shapeManager.HandleShapeMovement();
-        }
-    }
-
-    private void UpdateLevelIndex()
-    {
-        // set current level index to number of selected level - 1
+        
     }
 }
